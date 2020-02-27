3199adddbb039f376dbb53d4bf7f748eb97fc413
CIT230 Test Student Account <jac17047@byui.edu>
2020-02-26 20:44:22 -0500

    update

---
+++ lesson5/js/main.js
@@ -0,0 +1,47 @@
+let currentDate = new Date();
+
+let currentDateString;
+
+
+// get day of week
+let weekDayNumber = currentDate.getDay();
+let daysOfWeek =[
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+];
+let weekDay = daysOfWeek[weekDayNumber];
+currentDateString = weekDay + ', ';
+currentDateString  = currentDateString + currentDate.getDate();
+let months =[
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+];
+let monthNumber = currentDate.getMonth();
+let month = months[monthNumber];
+currentDateString += ' ' + month;
+currentDateString += ' ' + currentDate.getFullYear();
+document.getElementById('currentDate').innerHTML = currentDateString;
+console.log(currentDateString);
+
+function toggleMenu() {
+	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+if (weekDayNumber == 5){
+    document.getElementById("pancakes").removeAttribute("class","hidden");
+}
+

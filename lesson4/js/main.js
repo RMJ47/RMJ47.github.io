d818b7c90b557766fbb10856c46073e7ab7ea4da
CIT230 Test Student Account <jac17047@byui.edu>
2020-04-08 21:47:24 -0400

    update

--- lesson4/js/main.js
+++ lesson4/js/main.js
@@ -41,3 +41,28 @@ console.log(currentDateString);
 function toggleMenu() {
 	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
+
+/create 5 day forecast
+let currentDay = weekDayNumber;
+
+for (let i = 1; i < 6; i++) {
+    // add one day to current day
+    currentDay++;
+
+    // if current day is greater than 6 (Saturday), reset it to 0 (Sunday)
+    if (currentDay > 6) {
+        currentDay = 0;
+    }
+
+    // assing value to placeholder in HTML
+    const element = document.getElementById(`day${i}`);
+
+    element.innerHTML = daysOfWeek[currentDay];
+
+    // show/hide pancake
+    if (weekDayNumber == 5){
+        document.getElementById("pancake").removeAttribute("class", "hidden");
+    }
+    
+}
+
